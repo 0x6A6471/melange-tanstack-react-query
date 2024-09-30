d524dc610644a7a6454627af3e396a5ece59dfb6
Jake Quinter <hello@jakequinter.io>
2024-09-30 17:16:14 -0500

    revert

--- src/ReactQuery.ml
+++ src/ReactQuery.ml
@@ -148,19 +148,6 @@ type ('params, 'data, 'error, 'context) mutation_state =
   ; mutateAsync : 'params -> 'data Js.Promise.t
   }
 
-let make_mutation
-  ~mutationFn
-  ?retry
-  ?retryDelay
-  ?onMutate
-  ?onError
-  ?onSuccess
-  ?onSettled
-  ()
-  =
-  { mutationFn; retry; retryDelay; onMutate; onError; onSuccess; onSettled }
-;;
-
 external useMutation
   :  ('params, 'data, 'error, 'context) use_mutation_options
   -> ('params, 'data, 'error, 'context) mutation_state
